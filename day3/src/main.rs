2a34bdb29a7219921fe0c5c211de40814894d121
Nicholas LaJoie <nic@iotato.me>
2020-12-05 12:14:43 -0500

    Completed day 3 part 2!

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -9,33 +9,17 @@ fn main() {
         let args: Vec<String> = env::args().collect();
         let f = get_filename(&args);
 
-	// Variables we need
-	let tree = '#';
-	let mut cur_index = 0;
-	let mut tree_count = 0;
-
-	// Open file and start parsing
-	if let Ok(lines) = read_lines(f) {
-		for line in lines {
-			if let Ok(cur_line) = line {
-				// Check for trees!
-				let t = &cur_line.chars().nth(cur_index).unwrap(); 
-				if t.eq(&tree) {
-					tree_count += 1;
-				}
-
-				// Increment our counter!
-				if cur_index + 3 < cur_line.chars().count() {
-					cur_index += 3;
-				} else {
-					cur_index = (cur_index + 3) % cur_line.chars().count();
-				}
-			}
-		}
-	}
+	// Array of slopes: (right, down)
+	let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
 
 	// The result?
-	println!("Trees encountered: {}", tree_count);
+	let mut total = 1;
+	for slope in slopes.iter() {
+		let result = count_trees(slope, f);
+		total *= result;
+		println!("For slope ({}, {}), trees encountered: {}", slope.0, slope.1, result);	
+	}
+	println!("All together now: {}", total);
 }
 
 fn get_filename(args: &[String]) -> &str {
@@ -50,4 +34,36 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
         let file = File::open(filename)?;
         Ok(io::BufReader::new(file).lines())
+}
+
+fn count_trees(slope: &(usize, usize), file: &str) -> usize {
+
+	let tree = '#';
+	let mut cur_index = 0;
+	let mut tree_count = 0;
+	let mut line_num = 0;
+
+	// Open file and start parsing
+	if let Ok(lines) = read_lines(file) {
+		for line in lines {
+			if line_num % slope.1 == 0 { 
+				if let Ok(cur_line) = line {
+					// Check for trees!
+					let t = &cur_line.chars().nth(cur_index).unwrap(); 
+					if t.eq(&tree) {
+						tree_count += 1;
+					}
+
+					// Increment our counter!
+					if cur_index + slope.0 < cur_line.chars().count() {
+						cur_index += slope.0;
+					} else {
+						cur_index = (cur_index + slope.0) % cur_line.chars().count();
+					}
+				}
+			}
+			line_num += 1;
+		}
+	}
+	tree_count
 }
