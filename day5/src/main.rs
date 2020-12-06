a4134baefc51e11ef447f39e0eaf8e3a6efd7d4d
nic <nic@iotato.me>
2020-12-06 11:45:24 -0500

    Done day5 part 1! Eventually noticed the neat trick that gives you the seat number quite easily. This one was way more fun than yesterday. On to part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -15,11 +15,16 @@ fn main() {
     if let Ok(lines) = read_lines(f) {
         // Parse input line by line 
         // Thanks: https://riptutorial.com/rust/example/4275/read-a-file-line-by-line
+        let mut max_seat: i16 = 0;
         for (index, line) in lines.enumerate() {
             let line = line.unwrap();
-            println!("{}: {}", index + 1, line);
-            println!("{}", get_row(&line));
+            let s = get_seat(&line);
+            if s > max_seat {
+                max_seat = s;
+            }
+            println!("{}: {}", index + 1, s);
         }
+        println!("Max seat number: {}", max_seat);
     }
 }
 
@@ -37,32 +42,34 @@ where P: AsRef<Path>, {
         Ok(io::BufReader::new(file).lines())
 }
 
-fn get_row(input: &str) -> u8 {
-    let row: u8 = 255;
+fn get_seat(input: &str) -> i16 {
+    let mut seat: i16 = -1;
     let mut flag = true;
 
     // Validate the input
     if input.chars().count() != 10 {
         println!("INVALID: Incorrect # of characters"); 
         flag = false;
-    } 
-    for (i, c) in input.chars().enumerate() {
-        if i < 7 {
-            if c != 'F' && c != 'B' {
-                println!("INVALID: F/B section");
-                flag = false;
-            }
-        } else {
-            if c != 'R' && c != 'L' {
-                println!("INVALID: L/R section");
-                flag = false;
+    } else {
+        for (i, c) in input.chars().enumerate() {
+            if i < 7 {
+                if c != 'F' && c != 'B' {
+                    println!("INVALID: F/B section");
+                    flag = false;
+                }
+            } else {
+                if c != 'L' && c != 'R' {
+                    println!("INVALID: L/R section");
+                    flag = false;
+                }
             }
         }
     }
 
     // Determine the row using the first 7 chars of the input
     if flag == true {
-        println!("Looks good!");
+        let temp = input.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1");
+        seat = i16::from_str_radix(&temp, 2).unwrap();
     }
-    row
+    seat
 }
