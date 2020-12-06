75f240991e003ed53c5e38dbdab64b36d8fb032e
nic <nic@iotato.me>
2020-12-06 10:18:10 -0500

    Fix type in gitignore, printing all lines in day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,37 @@
+// Author: SpudNiche
+// Advent of Code, Day 5
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    // Read in cmd line args
+    let args: Vec<String> = env::args().collect();
+    let f = get_filename(&args);
+
+    // Open file
+    if let Ok(lines) = read_lines(f) {
+        // Parse input line by line 
+        // Thanks: https://riptutorial.com/rust/example/4275/read-a-file-line-by-line
+        for (index, line) in lines.enumerate() {
+            let line = line.unwrap();
+            println!("{}: {}", index + 1, line);
+        }
+    }
+}
+
+fn get_filename(args: &[String]) -> &str {
+	if args.len() != 2 {
+		panic!("Invalid arguments");
+	}
+	let filename = &args[1];
+	filename
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
 }
